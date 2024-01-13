b3955a4f7b0bd05c5e3beef6c7a7e3e49d60f1fb
Murad Gojayev <muradqocayevi20@gmail.com>
2024-01-13 10:25:29 +0400

    updated

--- audi/env.d.ts
+++ audi/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.png" {
+    const path: string;
+    export default path;
+}
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
+  
